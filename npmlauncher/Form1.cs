7e066540143ca167faa28c1469c176db0c62dba2
Georgii Dolzhykov <thorn.mailbox@gmail.com>
2017-11-07 21:28:58 +0300

    Show pkg version, add .editorconfig

--- npmlauncher/Form1.cs
+++ npmlauncher/Form1.cs
@@ -8,19 +8,15 @@ using System.Linq;
 using System.Web.Script.Serialization;
 using System.Windows.Forms;
 
-namespace npmlauncher
-{
-  public partial class Form1 : Form
-  {
-    public Form1()
-    {
+namespace npmlauncher {
+  public partial class Form1 : Form {
+    public Form1() {
       InitializeComponent();
       Icon = Icon.ExtractAssociatedIcon(System.Reflection.Assembly.GetExecutingAssembly().Location);
       Populate();
     }
 
-    private void Populate()
-    {
+    private void Populate() {
       var serializer = new JavaScriptSerializer();
       string fileName = "package.json";
       if (!File.Exists(fileName)) Exit();
@@ -34,32 +30,29 @@ namespace npmlauncher
         where !string.IsNullOrWhiteSpace(command)
         select new ScriptEntry(entry.Key, command)
       ).ToList();
-      Text = (pkg.ContainsKey("name") ? pkg["name"] + ": " : "") + "npm run …";
+      string name = pkg.ContainsKey("name") ? pkg["name"] as string : null;
+      if (pkg.ContainsKey("version") && !string.IsNullOrWhiteSpace(pkg["version"] as string)) {
+        name = (string.IsNullOrWhiteSpace(name) ? "" : name + " ") + "v" + pkg["version"];
+      }
+      Text = (string.IsNullOrWhiteSpace(name) ? "" : name + ": ") + "npm run …";
     }
 
-    private void listBox1_DoubleClick(object sender, EventArgs e)
-    {
+    private void listBox1_DoubleClick(object sender, EventArgs e) {
       var selected = (ScriptEntry)listBox1.SelectedItem;
       Process.Start("cmd", "/c npm run " + selected.Name);
       Exit();
     }
 
-    private void Exit()
-    {
-      if (Application.MessageLoop)
-      {
+    private void Exit() {
+      if (Application.MessageLoop) {
         Application.Exit();
-      }
-      else
-      {
+      } else {
         Environment.Exit(0);
       }
     }
 
-    private void listBox1_KeyPress(object sender, KeyPressEventArgs e)
-    {
-      switch (e.KeyChar)
-      {
+    private void listBox1_KeyPress(object sender, KeyPressEventArgs e) {
+      switch (e.KeyChar) {
         case (char)13:
           listBox1_DoubleClick(null, null);
           break;
